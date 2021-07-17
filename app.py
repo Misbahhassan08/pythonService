5239d978cecc18f3a105b4034bae8e639399e6bf
misbahhassan08 <misbahhassan08@gmail.com>
2021-07-18 00:29:16 +0500

    docker changes 2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 # ------------------------Imports----------------------------
 import os 
-from flask import Flask 
+from flask import Flask, request
 # ----------------------- Importing Global Initializer GniDyn
 
 # app name -------------------------------------------------
@@ -12,11 +12,11 @@ app = Flask(__name__)
 # routing --------------------------------------------------
 @app.route('/')
 def startApp():
-    #from GNI import GniDyn
-    from GNI import GniDyn
-    print('Returning now...')
-    return True
+    """ Return a friendly HTTP greeting. """
+    who = request.args.get("who", "World")
+    return f"Hello {who}!\n"
     pass
 
 if __name__ == "__main__":
+    from GNI import GniDyn
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
